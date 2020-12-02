93ba98f80dd6fb0ccd376c6999d3028e2c078646
SondosMatahen <sondosmatahen@gmail.com>
2020-12-02 20:09:46 +0200

    finish

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# weather
+# Weather-Journal App Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Prerequisite](#Prerequisite)
+* [Installation](#Installation)
+* [Runtime Environment](#Runtime)
+
+## Instructions
+
+TThis project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
+
+
+## Prerequisite
+
+To be able to read this code you should have idea about HTML , CSS ,JS(DOM)
+I used event listner to make the project more dynmaic.
+
+## Installation
+
+To get started you should have node.js.
+Then input to terminal `node server.js`.
+
+
+## Runtime Environment
+* JS6
+* DOM
---
+++ website/app.js
@@ -0,0 +1,85 @@
+/* Global Variables */
+let API = '0a024f54b02105bb92c0c2f07a371fc4';
+let form =document.getElementById('form')
+// let zip = document.getElementById('zip').value;
+// let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API}`;
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+
+document.getElementById('generate').addEventListener('click', handdleClick);
+
+function handdleClick(e) {
+    e.preventDefault();
+    let zip = document.getElementById('zip').value;
+    let content = document.getElementById('feelings').value;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API}&units=metric`;
+
+    getWeather(url)
+    .then(useData => {
+        console.log('getweather', useData);
+        postData('/add', { date: newDate, temp: useData.main.temp, content })
+    }).then(update=>{
+        updateUI();
+    });
+    form.reset();
+}
+
+// function to get weather data
+const getWeather = async (url) => {
+    console.log('url', url)
+    const res = fetch(url);
+    try {
+        // console.log('get weeeee', await res);
+        const data =  (await res).json();
+        // console.log('get we', data);
+        return data
+    } catch (e) {
+        console.log(e)
+    };
+
+};
+
+// function to post weather data
+const postData = async (url = '', data = {}) => {
+    const request = await fetch(url, {
+        method: 'POST',
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        },
+        body: JSON.stringify({
+            date: data.date,
+            temp: data.temp,
+            content: data.content
+        })
+    });
+
+    try {
+        const newData = await request.json();
+        console.log('new', newData);
+        return newData;
+    } catch (e) {
+        console.log('e', e);
+    };
+};
+
+
+//Function to update the data to user
+const updateUI = async () => {
+    const req = await fetch('/getAll');
+    try {
+        const addData = await req.json();
+        console.log('newwwww', addData);
+
+        document.getElementById('date').innerHTML = addData.date;
+        document.getElementById('temp').innerHTML = addData.tem;
+        document.getElementById('content').innerHTML = addData.content;
+    } catch (e) {
+        console.log('e', e);
+    };
+};
