3959937f3ee7921a52f28b3ce1559a877cb2d3c8
SondosMatahen <sondosmatahen@gmail.com>
2020-12-04 14:39:03 +0200

    edit date

--- website/app.js
+++ website/app.js
@@ -6,7 +6,7 @@ let form =document.getElementById('form')
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = d.getMonth() +1+ '.' + d.getDate() + '.' + d.getFullYear();
 
 
 
